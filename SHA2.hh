c334ba71a5625aefe0e843f0c3fb036e0a7ea7c4
Ali Reza <a.nameghi@outlook.com>
2018-06-09 17:20:13 +0430

    fixed a minor issue
    for test, see: https://wandbox.org/permlink/aro5C2K687rR9j2B

--- SHA2.hh
+++ SHA2.hh
@@ -136,32 +136,33 @@ namespace sha2
         class general_sha2
         {
         private:
-            hashdata <T, Hash_Bits> result;         /// result of hash
-            T const *round_table, *init_vector;     /// pointers to the round table, init vector,...
-            uint8_t const *sr;                      /// and shift|rotate values
+            hashdata <T, Hash_Bits> result;        /// result of hash
+            T const *round_table, *init_vector;    /// pointers to the round table, init vector,...
+            uint8_t const *sr;                     /// and shift|rotate values
 
         public:
             ///------ constructor and initializer
             inline general_sha2()
             {
-                if (sizeof(T) / 8)   /// is 64-bit
+                sr = (uint8_t const*)& shift_rotate_amounts[12 * (sizeof(T) / 8)];
+                if (sizeof(T) / 8)  /// is 64-bit
                 {
                     round_table = (T const*)(void*)& sha512_round_table[0];
                     init_vector = (T const*)(void*)& sha512_init_vectors[H - SHA_512][0];
+                    return;
                 }
-                else    /// for sha224(sha256), use the MSBs of round table, and LSB(MSB)s of I.V.
+                static uint32_t table_iv_32[80];
+                /// for sha224(sha256), use the MSBs of round table, and LSB(MSB)s of the I.V.
+                if (table_iv_32[0] == 0)
                 {
-                    static uint32_t table_iv_32[80];
-
-                    for (int i = 0; i < 80 && !table_iv_32[79]; i++)    /// Skip if already evaluated
+                    for (int i = 0; i < 80; i++)
                     {
                         table_iv_32[i] = i < 64 ? uint32_t(sha512_round_table[i] >> 32) :
                             uint32_t(sha512_init_vectors[i / 72][i % 8] >> int(i < 72) * 32);
                     }
-                    round_table = (T const*)(void*)& table_iv_32[0];
-                    init_vector = (T const*)(void*)& table_iv_32[8 * (H - SHA_256 + 8)];
                 }
-                sr = (uint8_t const*)& shift_rotate_amounts[12 * (sizeof(T) / 8)];
+                round_table = (T const*)(void*)& table_iv_32[0];
+                init_vector = (T const*)(void*)& table_iv_32[8 * (H - SHA_256 + 8)];
             }
 
             ///------ destructor
